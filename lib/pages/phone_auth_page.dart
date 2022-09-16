df1886039498081b7d01e85df6847898c89cb96e
Muhammad Umar <MuhammadUmar62792@gmail.com>
2022-09-16 21:29:39 +0500

    phone UI final version

--- lib/pages/phone_auth_page.dart
+++ lib/pages/phone_auth_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/utils/colors.dart';
+import 'package:country_picker/country_picker.dart';
 
-class PhoneAuthPage extends StatelessWidget {
+class PhoneAuthPage extends StatefulWidget {
   const PhoneAuthPage({super.key});
+
+  @override
+  State<PhoneAuthPage> createState() => _PhoneAuthPageState();
+}
+
+class _PhoneAuthPageState extends State<PhoneAuthPage> {
+  String countryCode = '';
+  String selectedCountry = 'Select Country';
+
+  countryPicker() {
+    showCountryPicker(
+      context: context,
+      onSelect: (Country country) {
+        setState(() {
+          countryCode = country.phoneCode;
+          selectedCountry = country.displayNameNoCountryCode;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,16 +46,24 @@ class PhoneAuthPage extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 25),
+        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(
+            SizedBox(
               width: double.infinity,
-              child: Text(
-                'Whatsapp will need to verify your phone number.',
-                textAlign: TextAlign.center,
-              ),
+              child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                      style: TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                            text:
+                                'WhatsApp will need to verify your phone number.'),
+                        TextSpan(
+                            text: " What's my number?",
+                            style: TextStyle(color: Colors.blue))
+                      ])),
             ),
             // select country
             Container(
@@ -45,17 +74,17 @@ class PhoneAuthPage extends StatelessWidget {
                 children: [
                   Expanded(child: Container()),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: countryPicker,
                     style: const ButtonStyle(),
-                    child: const Text(
-                      'Pakistan',
-                      style: TextStyle(
+                    child: Text(
+                      selectedCountry,
+                      style: const TextStyle(
                           color: Colors.black, fontSize: 15, height: 2),
                     ),
                   ),
                   Expanded(child: Container()),
                   IconButton(
-                      onPressed: () {},
+                      onPressed: countryPicker,
                       icon: Icon(
                         Icons.arrow_drop_down,
                         color: kWhatsAppColor,
@@ -72,10 +101,18 @@ class PhoneAuthPage extends StatelessWidget {
                   decoration: BoxDecoration(
                       border:
                           Border(bottom: BorderSide(color: kWhatsAppColor))),
-                  width: size.width * .18,
-                  child: const Text(
-                    '+   92',
-                    style: TextStyle(fontSize: 18, height: 3),
+                  width: size.width * .15,
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(color: Colors.black),
+                      children: [
+                        TextSpan(
+                          text: '+  ',
+                          style: TextStyle(color: Colors.grey[700], height: 3),
+                        ),
+                        TextSpan(text: countryCode, style: const TextStyle())
+                      ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -85,11 +122,12 @@ class PhoneAuthPage extends StatelessWidget {
                   decoration: BoxDecoration(
                       border:
                           Border(bottom: BorderSide(color: kWhatsAppColor))),
-                  width: size.width * .37,
+                  width: size.width * .40,
                   child: TextField(
                     decoration: InputDecoration(
                         hintText: 'phone number',
                         focusColor: kWhatsAppColor,
+                        contentPadding: const EdgeInsets.only(top: 15),
                         hoverColor: kWhatsAppColor),
                   ),
                 ),
@@ -97,6 +135,8 @@ class PhoneAuthPage extends StatelessWidget {
             ),
 
             Expanded(child: Container()),
+
+            //next button
             ElevatedButton(
               onPressed: () {},
               style: ButtonStyle(
